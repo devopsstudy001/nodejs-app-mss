1809b1bd9f383a3c99dd60db0abaf52ed8a67c94
devopsstudy001 <168479526+devopsstudy001@users.noreply.github.com>
2024-05-30 19:35:21 +0200

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,19 +1,17 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+const scanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
-  serverUrl: 'http://3.91.36.69:9000/	',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_f26b284073361a3cc483109742ae4b38fd5efd96',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
-}, () => {});
+scanner(
+  {
+    serverUrl: 'http://3.91.36.69:9000/',
+    token: "squ_f26b284073361a3cc483109742ae4b38fd5efd96",
+    options: {
+      'sonar.projectName': 'Node JS Application - Sample',
+      'sonar.projectKey': 'NodeJsMithunTechnologies',
+      'sonar.projectVersion': '1.0',
+      'sonar.sources': 'src',
+      'sonar.language': 'js',
+      'sonar.sourceEncoding': 'UTF-8',
+    },
+  },
+  () => process.exit()
+);
